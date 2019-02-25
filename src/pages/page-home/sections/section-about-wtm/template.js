37f126af9998c54d3b482b50856ebe61dce0bfbd
Stephen John Raymundo <bayan.mahal@live.com>
2019-02-25 23:42:19 +0800

    update about section

--- src/pages/page-home/sections/section-about-wtm/template.js
+++ src/pages/page-home/sections/section-about-wtm/template.js
@@ -7,10 +7,6 @@ const template = html => html`
     <p>Google believes diversity and inclusion is important for thriving vibrant communities. Focusing on women-inclusion is the first step in a larger effort to diversify the global Google developer community. The GDG Philippines community has shown great interest in this initiative, starting with GDG DevFest Women 2013, Women Techmakers 2014, and Women Techmakers 2015.</p>
     <p>Women Techmakers is Google's brand and global program for women in technology. Women Techmakers provides visibility, community, and resources for women in technology to drive innovation and participation in the field. Women Techmakers events are designed to promote an environment of inclusivity for women in technology, as well as celebrate women leading the industry. We want to advance, grow, and celebrate women developers, designers, and technology entrepreneurs.</p>
   
-    <h1>WTM 2018</h1>
-  
-    <p>Google believes diversity and inclusion is important for thriving vibrant communities. Focusing on women-inclusion is the first step in a larger effort to diversify the global Google developer community. The GDG Philippines community has shown great interest in this initiative, starting with GDG DevFest Women 2013, Women Techmakers 2014, and Women Techmakers 2015.</p>
-    <p>Women Techmakers is Google's brand and global program for women in technology. Women Techmakers provides visibility, community, and resources for women in technology to drive innovation and participation in the field. Women Techmakers events are designed to promote an environment of inclusivity for women in technology, as well as celebrate women leading the industry. We want to advance, grow, and celebrate women developers, designers, and technology entrepreneurs.</p>
   </div>
 
 `;
