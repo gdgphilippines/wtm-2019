07bfa0793bc0e271784db6e08e6f466e219dcbe6
Stephen John Raymundo <bayan.mahal@live.com>
2019-02-25 23:41:49 +0800

    update sidebar

--- src/modules/general/components/project-sidebar/template.js
+++ src/modules/general/components/project-sidebar/template.js
@@ -10,11 +10,11 @@ const template = (html, self) => function () {
       <navigation-wrapper>
         <side-navigation @close-sidebar=${close.bind(this)}>
           <ul class="links">
-            <li><a href="/?id=home" id="home-link" @click="${close.bind(this)}" class="active"><span>Home</span></a></li>
-            <li><a href="/?id=event-details" id="event-details-link" @click="${close.bind(this)}"><span>Event Details</span></a></li>
-            <li><a href="/?id=agenda" id="agenda-link" @click="${close.bind(this)}"><span>Agenda</span></a></li>
-            <li><a href="/?id=about-wtm" id="about-wtm-link" @click="${close.bind(this)}"><span>About WTM</span></a></li>
-            <li><a href="https://www.meetup.com/gdgphilippines/events/258563356/" target="_blank" rel="noopener" @click="${close.bind(this)}"><span>Cloud Study Jams</span></a></li>
+            <li><a  @click=${e => this.triggerScroll('home')}  id="home-link" class="active"><span>Home</span></a></li>
+            <li><a  @click=${e => this.triggerScroll('event-details')}  id="event-details-link"><span>Event Details</span></a></li>
+            <li><a  @click=${e => this.triggerScroll('agenda')}  id="agenda-link" ><span>Agenda</span></a></li>
+            <li><a  @click=${e => this.triggerScroll('about-wtm') }  id="about-wtm-link" ><span>About WTM</span></a></li>
+            <!-- <li><a href="https://www.meetup.com/gdgphilippines/events/258563356/" target="_blank" rel="noopener" @click="${close.bind(this)}"><span>Cloud Study Jams</span></a></li> -->
             <li><a href="https://docs.google.com/forms/d/1OFo4v-vKU_FNRr8Owuo0mCZMAkHHKl7rbyfumOSDb-A/edit?usp=drive_open" target="_blank" rel="noopener" class="active"><span>Register to Attend</span></a></li>
           </ul>
         </side-navigation>
