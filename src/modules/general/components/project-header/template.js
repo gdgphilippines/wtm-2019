d8cdcf379fcd03cefd3aa816e470fc62f365a260
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2019-03-14 14:00:32 +0800

    changed registration link

--- src/modules/general/components/project-header/template.js
+++ src/modules/general/components/project-header/template.js
@@ -22,7 +22,8 @@ const template = (html, self) => function () {
         <li class="link-item"><a @click=${e => this.triggerScroll('event-details')} data-id="section-event-details" class="link"><span class="span">Event Details</span></a></li>
         <li class="link-item"><a @click=${e => this.triggerScroll('agenda')} data-id="section-agenda" class="link"><span class="span">Agenda</span></a></li>
         <li class="link-item"><a @click=${e => this.triggerScroll('about-wtm')} data-id="section-about-wtm" class="link"><span class="span">About WTM</span></a></li>
-        <li class="link-item"><a target="_blank" rel="noopener" href="https://docs.google.com/forms/d/1OFo4v-vKU_FNRr8Owuo0mCZMAkHHKl7rbyfumOSDb-A/edit?usp=drive_open" class="link register"><span class="span">Register</span></a></li>
+        <li class="link-item"><a target="_blank" rel="noopener" href="https://docs.google.com/forms/d/e/1FAIpQLSdEh9FurGJzLTziQaoUXhjMjkKCWlkSxmlIn4fWaBS1mX7I3g/viewform
+" class="link register"><span class="span">Register</span></a></li>
         <!-- <li class="link-item"><a href="https://www.meetup.com/gdgphilippines/events/258563356/" rel="noopener" target="_blank" class="link"><span class="span">Cloud Study Jams</span></a></li> -->
       </ul>
 
