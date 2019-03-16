e81728115d7c61f9920fc82afcc14d0d0e8c53f5
Stephen John Raymundo <bayan.mahal@live.com>
2019-03-16 22:36:25 +0800

    update sponsors section

--- src/pages/page-home/sections/section-sponsors/template.js
+++ src/pages/page-home/sections/section-sponsors/template.js
@@ -1,6 +1,54 @@
 const template = html => html`
   
-  <h1> Sponsors </h1>
+<center>
+  <h3>Powered by</h3>
+  <div class="logo-container">
+    <lazy-picture class="sponsor-logo"
+      src="/assets/sponsor-logos/Copy of google.png"
+      alt="Google">
+    </lazy-picture>
+  </div>
+  <h3>Co-Presented by</h3>
+  <div class="logo-container">
+    <lazy-picture class="sponsor-logo-wide"
+        src="/assets/sponsor-logos/Acc_Technology_Lockup_BLK.png"
+        alt="Accenture">
+    </lazy-picture>
+  </div>
+  <h3>Made Possible by</h3>
+  <div class="logo-container">
+    <lazy-picture class="sponsor-logo"
+      src="/assets/sponsor-logos/Copy of Globe Vector Logo CMYK Pos V2.png"
+      alt="Globe">
+    </lazy-picture>
+    <lazy-picture class="sponsor-logo"
+      src="/assets/sponsor-logos/Globe Labs-new-positive.png"
+      alt="Globe Labs">
+    </lazy-picture>
+  </div>
+  <h3>Special Thanks to</h3>
+  <div class="logo-container">
+    <lazy-picture class="sponsor-logo"
+      src="/assets/sponsor-logos/Copy of bdj logo_pink.png"
+      alt="BDJ">
+    </lazy-picture>
+    <lazy-picture class="sponsor-logo"
+      src="/assets/sponsor-logos/Celeteque DermoScience_1.jpg"
+      alt="Celeteque">
+    </lazy-picture>
+  </div>
+  <h3>Official Media Partners</h3>
+  <div class="logo-container">
+    <lazy-picture class="sponsor-logo"
+      src="/assets/sponsor-logos/Copy of ANC_logo_reg_FINAL.JPG"
+      alt="ANC">
+    </lazy-picture>
+    <lazy-picture class="sponsor-logo-narrow"
+      src="/assets/sponsor-logos/Copy of wheninmanila.png"
+      alt="Celeteque">
+    </lazy-picture>
+  </div>
+</center>
   
 
 `;
