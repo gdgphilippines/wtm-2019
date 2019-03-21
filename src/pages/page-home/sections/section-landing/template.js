d87eb8ad5e95a65b2c104d40a6974611524b6924
Stephen John Raymundo <bayan.mahal@live.com>
2019-03-21 18:04:16 +0800

    update CTA

--- src/pages/page-home/sections/section-landing/template.js
+++ src/pages/page-home/sections/section-landing/template.js
@@ -29,10 +29,7 @@ const template = html => html`
       Google's <b>Women Techmakers</b> program provides visibility,
       community, and resources for women in technology
     </p>
-    <a class="button" target="_blank" rel="noopener" href="https://docs.google.com/forms/d/e/1FAIpQLSdEh9FurGJzLTziQaoUXhjMjkKCWlkSxmlIn4fWaBS1mX7I3g/viewform
-">
-      Register
-    </a>
+    <a onclick="window.scrollTo(0, window.innerHeight)" class="button">KNOW MORE ABOUT THE EVENT</a>
     <!-- https://www.meetup.com/gdgphilippines/events/258563356/ -->
   </div>
 </div>
